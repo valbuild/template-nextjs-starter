f1b3dfd24d472e2130dd8fc37c34e19e112add64
Fredrik Ekholdt <freekh@gmail.com>
2026-06-20 16:05:48 +0200

    Use suspend as default

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ValProvider config={config}>
+        <ValProvider config={config} suspend>
           <ValModulesClient />
           {children}
         </ValProvider>
